21dd1371ca388ac4eddce5ad72b098912410310a
Ye Khaing Win <yekhaingwinn@gmail.com>
2026-02-20 12:33:33 +0800

    feat: Implement WebSocket health checks to terminate unresponsive connections and refine broadcast handling.

--- src/ws/server.ts
+++ src/ws/server.ts
@@ -2,6 +2,9 @@ import { WebSocket, WebSocketServer } from 'ws';
 import http from 'http';
 import type { Match } from '../db/schema.js';
 
+export interface CustomWebSocket extends WebSocket {
+    isAlive: boolean;
+}
 
 export function sendJson(socket: WebSocket, payload: any) {
     if (socket.readyState !== socket.OPEN) {
@@ -13,21 +16,41 @@ export function sendJson(socket: WebSocket, payload: any) {
 
 export function broadcast(wss: WebSocketServer, payload: any) {
     for (const client of wss.clients) {
-        sendJson(client, payload);
-    }
+        if (client.readyState !== client.OPEN) {
+            continue;
+        }
+        client.send(JSON.stringify(payload));
+    } 
 }
 
 
 export function attachWss(server: http.Server) {
     const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024 });
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', (ws) => {
+        const socket = ws as CustomWebSocket;
+        socket.isAlive = true;
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        })
+        
         sendJson(socket, { type: 'welcome', message: 'Welcome to the Sportz API' });
 
         socket.on('error', (err) => {
             console.error('WebSocket error:', err);
         })
     })
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            const socket = ws as CustomWebSocket;
+            if (socket.isAlive === false) {
+                return socket.terminate()
+            }
+            socket.isAlive = false;
+            socket.ping(() => {})
+        })
+    }, 30000)
 
     function broadcastMatchCreated(match: Match) {
         broadcast(wss, { type: 'match.created', data: match })
