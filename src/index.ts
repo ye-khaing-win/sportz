e29b8692524ab86a2cbd1487b611a818e21f4e83
Ye Khaing Win <yekhaingwinn@gmail.com>
2026-03-02 07:33:12 +0800

    feat: Implement Arcjet security middleware for HTTP and WebSocket connections, including bot detection and rate limiting.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { matchRouter } from './routes/matches.js';
 import http from 'http'
 import { attachWss } from './ws/server.js';
+import { securityMiddleware } from './arcjet.js';
 
 const PORT = Number(process.env.PORT) || 8000;
 const HOST = process.env.HOST || '0.0.0.0';
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Sportz API' });
 });
+
+app.use(securityMiddleware())
 
 app.use('/matches', matchRouter);
 
